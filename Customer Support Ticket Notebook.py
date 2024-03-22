e6787f838e32ba5c330b59bb4203fc313750ad54
Faith Were <awuorfaith120@gmail.com>
2024-03-22 21:00:12 +0300

    Create Customer Support Ticket Notebook.py

---
+++ Customer Support Ticket Notebook.py
@@ -0,0 +1,9 @@
+#Importing the necessary libraries
+import pandas as pd
+import  numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Reading in our data
+df = pd.read_csv("C:\Users\Faith\Documents\GitHub\Customer-Support-Ticket\customer_support_tickets.csv")
+df.head(5)
