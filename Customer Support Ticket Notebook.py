5838258e766b48c8880562845b07f94034ee9715
Faith Were <awuorfaith120@gmail.com>
2024-03-23 23:09:30 +0300

    Understanding our data
    This is basic understanding of our data where we explored our dataset to get conversant before we perform analysis.

--- Customer Support Ticket Notebook.py
+++ Customer Support Ticket Notebook.py
@@ -5,5 +5,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Reading in our data
-df = pd.read_csv("C:\Users\Faith\Documents\GitHub\Customer-Support-Ticket\customer_support_tickets.csv")
+df = pd.read_csv(r"C:\Users\Faith\Documents\GitHub\Customer-Support-Ticket\customer_support_tickets.csv")
 df.head(5)
+
+#Checking on the data types
+df.dtypes
+
+#Summary statistics
+df.describe().T
+
+#Checking on the null values
+df.isnull().sum()
+
+#Checking on the duplicates
+df.duplicated().sum()
