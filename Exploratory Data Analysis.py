e6fc68a8e7b8302279bd60dcb0f498cfae3ac29f
Faith Were <awuorfaith120@gmail.com>
2024-03-25 21:57:21 +0300

    Update Exploratory Data Analysis.py

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -22,3 +22,30 @@ df_ticket['Customer Satisfaction Rating'] = df_ticket['Customer Satisfaction Rat
 
 #Now checking if we still have missing values 
 df.isnull().sum()
+
+#Distribution of customer age
+print(df_ticket['Customer Age'].max())
+print(df_ticket['Customer Age'].min())
+
+#The range of customer age from 18 to 70 years old.
+#We will classify them into 3 kinds of customer: Young, Middle Age and Old
+
+age = []
+for i in df_ticket['Customer Age']:
+    if i<=30:
+        age.append('Young Customer')
+    elif 30<i<55:
+        age.append('Middle Age Customer')
+    else:
+        age.append('Old Customer')
+df_ticket['Type of Customer'] = age
+
+#Plotting the age distribution
+import matplotlib.pyplot as plt
+
+chart_age = df_ticket['Type of Customer'].value_counts()
+chart_gen = df_ticket['Customer Gender'].value_counts()
+plt.figure(figsize = (10,5))
+plt.pie(chart_age, labels = chart_age.index, autopct='%.0f%%')
+plt.title('Distribution of customer age', loc = 'left', pad = 10, size = 15)
+plt.show()
