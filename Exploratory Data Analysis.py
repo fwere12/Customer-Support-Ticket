16f833078e59bce53b1a36d713ee26aafa8ebe9a
Faith Were <awuorfaith120@gmail.com>
2024-03-25 21:50:55 +0300

    Create Exploratory Data Analysis.py
    We first clean our dataset to ensure consistency within our dataset and enable us achieve reliable results from our analysis.

---
+++ Exploratory Data Analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Faith\Documents\GitHub\Customer-Support-Ticket\customer_support_tickets.csv")
+df.head(5)
+
+#Cleaning our data
+
+#Convert to datetime
+df['Date of Purchase'] = pd.to_datetime(df['Date of Purchase'], format = '%Y-%m-%d')
+df['First Response Time'] = pd.to_datetime(df['First Response Time'], format = '%Y-%m-%d %H:%M:%S')
+df['Time to Resolution'] = pd.to_datetime(df['Time to Resolution'], format = '%Y-%m-%d %H:%M:%S') 
+
+#Droping the null values
+df_ticket = df.drop(columns = ['Ticket ID'], axis = 1)
+#Because Ticket ID column is similar to index of the table, we drop it.
+
+#We replace the following missing values
+df_ticket['Resolution'] = df_ticket['Resolution'].fillna('None')
+df_ticket['First Response Time'] = df_ticket['First Response Time'].fillna('No response')
+df_ticket['Time to Resolution'] = df_ticket['Time to Resolution'].fillna('No resolution')
+df_ticket['Customer Satisfaction Rating'] = df_ticket['Customer Satisfaction Rating'].fillna('No rating')
+
+#Now checking if we still have missing values 
+df.isnull().sum()
