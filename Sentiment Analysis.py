8799aa4c1a4ab058dc30c0a0217b4cffbb22980b
Faith Were <awuorfaith120@gmail.com>
2024-03-31 21:44:39 +0300

    Create Sentiment Analysis.py
    This part will focus on text in Ticket Description columns and get some interesting insights from them. I will judge the text severity of ticket description base on ticket prority, ticket subject and whether it were resolved.

---
+++ Sentiment Analysis.py
@@ -0,0 +1,37 @@
+#Importing the necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv(r"C:\Users\Faith\Documents\GitHub\Customer-Support-Ticket\customer_support_tickets.csv")
+df.head(5)
+
+#Cleaning our data
+
+#Convert to datetime
+df['Date of Purchase'] = pd.to_datetime(df['Date of Purchase'], format = '%Y-%m-%d')
+df['First Response Time'] = pd.to_datetime(df['First Response Time'], format = '%Y-%m-%d %H:%M:%S')
+df['Time to Resolution'] = pd.to_datetime(df['Time to Resolution'], format = '%Y-%m-%d %H:%M:%S') 
+
+#Droping the null values
+df_ticket = df.drop(columns = ['Ticket ID'], axis = 1)
+#Because Ticket ID column is similar to index of the table, we drop it.
+
+#We replace the following missing values
+df_ticket['Resolution'] = df_ticket['Resolution'].fillna('None')
+df_ticket['First Response Time'] = df_ticket['First Response Time'].fillna('No response')
+df_ticket['Time to Resolution'] = df_ticket['Time to Resolution'].fillna('No resolution')
+df_ticket['Customer Satisfaction Rating'] = df_ticket['Customer Satisfaction Rating'].fillna('No rating')
+
+chance_r = []
+for i in df_ticket['Time to Resolution']:
+    if i == 'No resolution':
+        chance_r.append('No')
+    else:
+        chance_r.append('Yes')
+df_ticket['Resolution_bin'] = chance_r
+
+#a) Creating a new dataframe
+df_sentiment = df_ticket[['Ticket Description', 'Resolution', 'Ticket Priority', 'Ticket Subject', 'Resolution_bin']]
+df_sentiment.head()
